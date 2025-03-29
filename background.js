e98e5e78f83dc276807cca845316cf94bb4986a5
unknown <franklinmarquez9@gmail.com>
2025-03-29 12:40:49 -0400

    feat: new listerner using chrome runtime api

--- background.js
+++ background.js
@@ -1,17 +1,26 @@
 async function request() {
     try {
         const response = await fetch('https://pydolarve.org/api/v1/dollar')
-
+        
         if (!response.ok) {
             const errorMessage = `Error: ${response.status} - ${response.statusText}`
             throw new Error(errorMessage) 
         }
     
         const data = await response.json()
-        chrome.runtime.sendMessage({success: true, data})
+        return { success: true, data}
         
     } catch (error) {
         console.error('Fetch error: ', error)
-        chrome.runtime.sendMessage({ success: false, data})
+        return { success: false, error: error.message}
     }
 }
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "fetchDollarData") {
+        request().then((result) => {
+            sendResponse(result)
+        })
+        return true
+    }
+})

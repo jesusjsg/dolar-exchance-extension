394bd6ba9042c30b402577bf33f671adb791dd87
unknown <franklinmarquez9@gmail.com>
2025-03-29 12:40:10 -0400

    feat: new sendMessage to the background file

--- popup/popup.js
+++ popup/popup.js
@@ -3,51 +3,31 @@ const selectBtn = document.querySelector(".select-btn")
 const options = document.querySelectorAll(".options .option")
 const btnText = document.querySelector(".btn-text")
 const currentValue = document.querySelector("#current-dolar")
-const url = "https://pydolarve.org/api/v1/dollar"
 
-
-selectBtn.addEventListener("click", () => {
-    tasasMenu.classList.toggle("active")
-})
-
-options.forEach((option) => {
-    option.addEventListener("click", () => {
-        let selectedOption = option.querySelector(".option-text").innerText
-        btnText.innerText = selectedOption
-        tasasMenu.classList.remove("active")
-    })
-})
-
-function main() {
-    console.log(url)
-    try {
-        const tasas = request(url)
-        
-        tasas
-        .then((data) => {
-            currentValue.innerHTML = data.monitors.bcv.price.toFixed(2)
-        })
-
-    } catch (error) {
-        console.error(`Error fetching dollar price: ${error}`)
-    }
-}
-
-async function request(api) {
-    console.log(api)
-    try {
-        const response = await fetch(api)
-
-        if (response.ok) {
-            const data = await response.json()
-            return data
-        } else {
-            throw new Error('Network response was not ok.')
+document.addEventListener('DOMContentLoaded', () => {
+    chrome.runtime.sendMessage({ action: 'fetchDollarData'}, (response) => {
+        if (chrome.runtime.lastError) {
+            console.error('Runtime error: ', chrome.runtime.lastError.message)
+            console.error('Error while connecting to the background')
+            return
         }
 
-    } catch (error) {
-        throw new Error(`Failed to read the API: ${error}`);
-    }
-}
+        if (response && response.success) {
+            console.log(`The response is: ${response.data.monitors.bcv.price}`)
+        } else {
+            console.error('Error fetching data: ', response ? response.error : 'Unknown error')
+        }
+    })
 
-document.addEventListener('DOMContentLoaded', main)
+    selectBtn.addEventListener('click', () => {
+        tasasMenu.classList.toggle('active')
+    })
+
+    options.forEach((option) => {
+        option.addEventListener('click', () => {
+            let optionValue = option.querySelector('.option-text').innerText
+            btnText.innerText = optionValue
+            tasasMenu.classList.remove('active')
+        })
+    })
+})

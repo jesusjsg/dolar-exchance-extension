39ca9de3fe7b7ad7bcd8ffd3bca375469d7a097f
jesus <jesusjsg@proton.me>
2025-03-28 22:53:02 -0400

    feat: new host permission in manifest.json and new function to fetch the api

--- background.js
+++ background.js
@@ -1,4 +1,17 @@
-const url = "https://pydolarve.org/api/v1/dollar"
+async function request() {
+    try {
+        const response = await fetch('https://pydolarve.org/api/v1/dollar')
 
-
-chrome.runtime
+        if (!response.ok) {
+            const errorMessage = `Error: ${response.status} - ${response.statusText}`
+            throw new Error(errorMessage) 
+        }
+    
+        const data = await response.json()
+        chrome.runtime.sendMessage({success: true, data})
+        
+    } catch (error) {
+        console.error('Fetch error: ', error)
+        chrome.runtime.sendMessage({ success: false, data})
+    }
+}

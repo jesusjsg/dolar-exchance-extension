56885c4bf1f7a78db7bcd7764c85e32dbceb4a22
jesus <jesusjsg@proton.me>
2025-04-01 23:25:39 -0400

    feat: new rate added and new toggle button forthe dark mode

--- popup/popup.js
+++ popup/popup.js
@@ -3,21 +3,10 @@ const selectBtn = document.querySelector(".select-btn")
 const options = document.querySelectorAll(".options .option")
 const btnText = document.querySelector(".btn-text")
 const currentValue = document.querySelector("#current-dolar")
+const toggleColorMode = document.querySelector('.theme-switch input[type="checkbox"]')
 
 document.addEventListener('DOMContentLoaded', () => {
-    chrome.runtime.sendMessage({ action: 'fetchDollarData'}, (response) => {
-        if (chrome.runtime.lastError) {
-            console.error('Runtime error: ', chrome.runtime.lastError.message)
-            console.error('Error while connecting to the background')
-            return
-        }
-
-        if (response && response.success) {
-            console.log(`The response is: ${response.data.monitors.bcv.price}`)
-        } else {
-            console.error('Error fetching data: ', response ? response.error : 'Unknown error')
-        }
-    })
+    fetchDollarData('bcv')    
 
     selectBtn.addEventListener('click', () => {
         tasasMenu.classList.toggle('active')
@@ -25,9 +14,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
     options.forEach((option) => {
         option.addEventListener('click', () => {
-            let optionValue = option.querySelector('.option-text').innerText
-            btnText.innerText = optionValue
+            let optionValue = option.querySelector('.option-text').getAttribute('data-tasa') 
+            btnText.innerText = option.querySelector('.option-text').innerText
             tasasMenu.classList.remove('active')
+            fetchDollarData(optionValue)
         })
     })
+    toggleColorMode.addEventListener('change', toggleMode, false)
 })
+
+function fetchDollarData(rate) {
+    chrome.runtime.sendMessage({ action: 'fetchDollarData' }, (response) => {
+        if (chrome.runtime.lastError) {
+            console.error('Runtime error: ', chrome.runtime.lastError.message)
+            return
+        }
+        
+        if (response && response.success) {
+            const price = response.data.monitors[rate].price
+            currentValue.textContent = `La tasa ${rate} tiene un valor de ${price}`
+        } else {
+            console.error('Error fetching the data: ', response ? response.error : 'Unknown error')
+        } 
+    })
+}
+
+function toggleMode(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+}
